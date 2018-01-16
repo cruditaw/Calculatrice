4ebda153c7bb9880b11d7664f8fcfb6d2e003d60
cruditaw <zionhearth@gmail.com>
2018-01-16 05:03:04 -0800

    Initial commit

---
+++ app/src/main/java/com/example/cdsm/calculatrice/Dictionnary.java
@@ -0,0 +1,17 @@
+package com.example.cdsm.calculatrice;
+
+/**
+ * Created by cdsm on 1/16/18.
+ */
+
+public class Dictionnary {
+
+
+    public enum Operations {
+        ADDITION,
+        SUBSTRACTION,
+        MUTIPLICATION,
+        DIVISION,
+        MODULO
+    }
+}
---
+++ app/src/main/java/com/example/cdsm/calculatrice/MathOperations.java
@@ -0,0 +1,17 @@
+package com.example.cdsm.calculatrice;
+
+/**
+ * Created by cdsm on 1/16/18.
+ */
+
+public class MathOperations {
+
+    private int value;
+    private Dictionnary.Operations operation;
+
+    public void computeAddition() {
+        
+    }
+
+
+}
